1855c6e801b29ff44b839552a33cf8cb17dbf260
JCRaymond <raymond.jacob17@gmail.com>
2017-10-03 16:28:54 -0400

    Add ability to edit movie entry

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,17 @@ class MoviesController < ApplicationController
         redirect_to movies_path
     end
 
+    def edit
+        @movie = Movie.find params[:id]
+    end
+
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(movie_params)
+        flash[:notice] = "#{@movie.title} was successfully updated"
+        redirect_to movie_path(@movie)
+    end
+
     private
     def movie_params
         params.require(:movie).permit(:title, :rating, :description, :release_date)
