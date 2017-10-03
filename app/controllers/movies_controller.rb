558d0c2cffdd783e3fc065ce8bbe5eed375b8b4c
JCRaymond <raymond.jacob17@gmail.com>
2017-10-03 16:15:42 -0400

    Add movie record creation form and notice

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,19 @@ class MoviesController < ApplicationController
         @movie= Movie.find(id)
     end
 
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.create!(movie_params)
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+
+    private
+    def movie_params
+        params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+
 end
